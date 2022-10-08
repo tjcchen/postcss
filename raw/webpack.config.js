ade6e623c6d4fa7a8f35792de54875fba55836cd
Andy Chen <chenyangpoppin@hotmail.com>
2022-10-08 22:23:51 +0800

    feat: create a raw html folder

---
+++ raw/webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  module: {
+    rules: [{
+      test: /\.css$/,
+      exclude: /node_modules/,
+      use: [{
+          loader: 'style-loader',
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1,
+          }
+        },
+        {
+          loader: 'postcss-loader'
+        }
+      ]
+    }]
+  }
+};
