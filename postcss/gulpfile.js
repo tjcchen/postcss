00d67129bb2ec2585eef3e0acf4ac8ab8488fdd6
Andy Chen <chenyangpoppin@hotmail.com>
2022-10-12 21:40:12 +0800

    feat: utiliza cssnano to compress css

--- postcss/gulpfile.js
+++ postcss/gulpfile.js
@@ -2,13 +2,20 @@
  * Entry point file for gulp demo
  */
 const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
 
 gulp.task('styles', () => {
+  const plugins = [
+    cssnano()
+  ];
+
   return gulp
     .src('styles.css')
+    .pipe(postcss(plugins))
     .pipe(gulp.dest('./dest'));
 });
 
 gulp.task('watch:styles', () => {
-  gulp.watch('**/*.css', ['styles']);
+  gulp.watch('**/*.css', gulp.series('styles'));
 });
