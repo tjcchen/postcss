1e21953361643f19f0988aedb9838902bbfc54d5
Andy Chen <chenyangpoppin@hotmail.com>
2022-10-13 20:03:46 +0800

    feat: adding stylus support

--- postcss/gulpfile.js
+++ postcss/gulpfile.js
@@ -5,7 +5,8 @@ const gulp = require('gulp');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-const sass = require('gulp-sass')(require('sass'))
+const sass = require('gulp-sass')(require('sass'));
+// const stylus = require('gulp-stylus');
 
 gulp.task('styles', () => {
   const processors = [
@@ -16,12 +17,13 @@ gulp.task('styles', () => {
   ];
 
   return gulp
-    .src('styles.scss')
+    .src('styles.styl')
+    // .pipe(stylus())         // CSS preprocessor
     .pipe(sass())              // CSS preprocessor
     .pipe(postcss(processors)) // postcss
     .pipe(gulp.dest('./dest'));
 });
 
 gulp.task('watch:styles', () => {
-  gulp.watch('**/*.scss', gulp.series('styles'));
+  gulp.watch('**/*.styl', gulp.series('styles'));
 });
